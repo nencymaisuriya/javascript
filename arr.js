582ba41b0979794d3aea8e3cf6d3ea5158d57680
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-12-13 18:43:29 +0530

    13/12/2024

---
+++ arr.js
@@ -0,0 +1,33 @@
+//array=>store multiple value in single variavble(index start with 0)
+//let arr=[2,3,4,5,6];
+//console.log(arr);
+
+//array access => index number data Store 
+//console.log(arr[3])
+
+//types:-object ,non-primitive datatype
+//method:-check array format ture/flase
+//console.log(array.isarray(arr))
+//property array=>
+ //console.log((Array.length))
+//total number of length return 
+
+//method/function
+//Push,pop,shift,unshift,splice
+//1.push=always index added in last
+//let data=[11,22,33,44,55]
+//console.log(data);
+//data.push(55);
+//console.log(data);
+
+//2.pop=remove last index
+//data.pop()
+
+//3.shift=remove first index
+//data.shift()
+
+//4.unshift=add first index
+//data.unshift()
+
+//splice=remove any position
+//data.splice(2,2)
