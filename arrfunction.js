e3dc7ea9d2b93b131785bddf58c6be20843cccc6
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-12-16 18:56:34 +0530

    16/12/24

---
+++ arrfunction.js
@@ -0,0 +1,53 @@
+//higher  order method
+//map,filter,find,findindex,some,every,includes,indexof,foreach,map
+//filter():=filter the array value
+let data=[1,2,3,4,5]
+// let arr=data.filter((ele,index)=>{
+//     return ele>=3;
+// });
+// console.log(arr);
+
+//find():=findout element in array
+// let arr=data.find((ele,index)=>{
+//     return ele===4
+// });
+// console.log(arr);
+
+//findindex():= find index in array
+// let arr=data.findIndex((ele,index)=>{
+//     return ele===2;
+// });
+// console.log(arr);
+
+//some():=ture /flase return in array
+// let arr=data.some((ele,index)=>{
+//     return ele>=130;
+// });
+// console.log(arr);
+
+//every():=every cond check and cond flase = flase return = either true;
+ // let arr=data.every((ele,index)=>{
+    //  return ele>=1;
+    //  });
+    // console.log(arr);
+
+//includes():=index value search in array
+// let arr=data.includes((ele,index)=>{
+//          return ele===2;
+//      });
+//      console.log(arr);  
+
+//indexof():=find index value / number
+// console.log(data.indexOf(2))
+
+//map & foreach
+//foreach():=ittreting a cond
+// data.forEach((ele,index)=>{
+//     console.log(ele)
+// });
+
+//map():=no return show new array
+// let arr=data.map((ele,index)=>{
+//     return ele*4;
+// })
+// console.log(arr)
