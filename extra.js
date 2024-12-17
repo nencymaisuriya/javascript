1d6d14b9c73533f6341d8944b24960b4d5ad7b28
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-12-17 18:40:55 +0530

    17/12/24

---
+++ extra.js
@@ -0,0 +1,37 @@
+//string`:= collection of character("",'')
+// let a="hello world's"
+// console.log(a)
+// let data='javascript is "best"'
+// console.log(data)
+//slice():-
+// let data="my name is nency"
+// let a=data.slice(1,6);
+// console.log(a)
+//substring():-
+// let data="my name is nency"
+// let a=data.substring(0,10)
+// console.log(a);
+//split():-
+// let data="my name is nency"
+// let a=data.split("nency")
+// console.log(a);
+//trim():-
+// let data="  my name is nency  "
+// let a=data.trim()
+// console.log(a);
+
+//math():-
+//1.math.floor():-small value
+// let a=Math.floor(23.5)
+// console.log(a)
+
+//2.math.ceil():-big value
+// let a=Math.ceil(21.6)
+// console.log(a)
+
+//3.math.round():-round value
+// let a=Math.round(24.7)
+// console.log(a)
+
+//4.math.random():-random value return
+// let a=Math.random()*10
